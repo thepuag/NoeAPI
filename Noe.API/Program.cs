704cda4cf4c660aebfd5179318e4d4eb15cf41b0
Noe <noe.m.garc@gmail.com>
2025-06-14 15:47:21 +0200

    Inicio gitignore

--- Noe.API/Program.cs
+++ Noe.API/Program.cs
@@ -1,3 +1,4 @@
+// Program.cs actualizado con comprobación y aplicación de migraciones
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
@@ -11,42 +12,40 @@ using Noe.BLL.Mappings;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configuración de cadena de conexión según entorno
+var environment = builder.Environment.EnvironmentName;
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
+
+if (string.IsNullOrEmpty(connectionString))
+    throw new Exception("Cadena de conexión no encontrada");
+
+// Servicios base
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Database
+// DB Context
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-//         sqlOptions =>
-//         {
-//             sqlOptions.EnableRetryOnFailure(
-//                 maxRetryCount: 5,
-//                 maxRetryDelay: TimeSpan.FromSeconds(10),
-//                 errorNumbersToAdd: null);
-//         }));
+    options.UseSqlServer(connectionString));
 
 // AutoMapper
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
-// Repositories
+// Repositorios
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddScoped<IProductoRepository, ProductoRepository>();
 builder.Services.AddScoped<IAlmacenRepository, AlmacenRepository>();
 
-// Services
+// Servicios
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 builder.Services.AddScoped<IProductoService, ProductoService>();
 builder.Services.AddScoped<IAlmacenService, AlmacenService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 
-// JWT Authentication
-var jwtKey = builder.Configuration["Jwt:Key"];
-var key = Encoding.ASCII.GetBytes(jwtKey);
+// JWT
+var jwtKey = builder.Configuration["Jwt:Key"] ?? Environment.GetEnvironmentVariable("Jwt__Key");
+var key = Encoding.ASCII.GetBytes(jwtKey!);
 
 builder.Services.AddAuthentication(x =>
 {
@@ -73,18 +72,23 @@ builder.Services.AddAuthentication(x =>
 // CORS
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(builder =>
-    {
-        builder.WithOrigins("http://localhost:4200") // Angular default port
-               .AllowAnyHeader()
-               .AllowAnyMethod()
-               .AllowCredentials();
-    });
+#if DEBUG // Desarrollo
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials());
+#else    // Producción
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+#endif
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Swagger solo en desarrollo
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -96,5 +100,13 @@ app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+// Migraciones automáticas
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    Console.WriteLine("Aplicando migraciones pendientes si las hay...");
+    db.Database.Migrate();
+}
 
 app.Run();
